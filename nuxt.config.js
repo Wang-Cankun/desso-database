c1d2109e80d0c901b92808fa027b2da5a2f76a5b
Cankun Wang <flykun0620@gmail.com>
2020-02-13 16:08:16 -0500

    nuxt complete

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import EventService from './services/EventService.js'
+
 export default {
   mode: 'universal',
   /*
@@ -55,5 +57,14 @@ export default {
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
+  },
+  generate: {
+    routes: () => {
+      return EventService.getEvents().then((response) => {
+        return response.data.map((event) => {
+          return '/event/' + event.id
+        })
+      })
+    }
   }
 }
