3cb96b4121222d90cd4782b6ba052ffe315da9d7
Cankun Wang <flykun0620@gmail.com>
2020-02-26 16:56:15 -0500

    test markdown loading toc

--- pages/help/_id.vue
+++ pages/help/_id.vue
@@ -1,23 +1,30 @@
 /* eslint-disable vue/no-v-html */
+
 <template>
   <v-responsive class="mx-auto overflow-visible" max-width="1024">
     <v-container class="markdown-body">
-      <template lang="md">
-        # Hello World!
-        Current route is: {{ $route.path }}
-      </template>
-
       <div v-html="post"></div>
+      <Fab></Fab>
+      {{ toc }}
     </v-container>
   </v-responsive>
 </template>
 
 <script>
-// import hello from '@/static/docs/1.md'
+import Fab from '@/components/Fab'
+
 export default {
+  components: {
+    Fab
+  },
   async asyncData({ params, error }) {
     try {
       const post = await import(`@/static/docs/${params.id}.md`)
+      const navStart = post.default.lastIndexOf('<nav')
+      const navEnd = post.default.lastIndexOf('nav>') + 4
+      const navContent = post.default.substring(navStart, navEnd)
+
+      console.log(navContent)
       return {
         post: post.default
       }
@@ -27,8 +34,17 @@ export default {
   },
   data() {
     return {
-      post: null,
-      model: '# Hello World!'
+      post: null
+    }
+  },
+  computed: {
+    toc() {
+      return 1
+    }
+  },
+  methods: {
+    toLink(pos) {
+      this.$vuetify.goTo(0)
     }
   }
 }
@@ -41,5 +57,15 @@ export default {
 .v-application code::after,
 .v-application code::before {
   content: none;
+}
+.table-of-contents {
+  background: #f9f9f9 none repeat scroll 0 0;
+  border: 1px solid #aaa;
+  display: table;
+  font-size: 95%;
+  margin-bottom: 1em;
+  padding: 32px 0 0;
+  margin: 0;
+  width: 100%;
 }
 </style>
