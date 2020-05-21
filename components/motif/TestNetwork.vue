fab74070a35718227df88058937487a8a9ff82f0
Cankun Wang <flykun0620@gmail.com>
2020-05-21 19:32:06 -0400

    Add cytoscape test example

--- components/motif/TestNetwork.vue
+++ components/motif/TestNetwork.vue
@@ -38,53 +38,488 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 const config = {
   elements: [
     {
-      data: { id: 'ARID1A' },
-      position: { x: 489, y: 182 },
+      data: { id: 'ARID1A', group: 'tf_source', type: 'triangle' },
+      position: { x: 350, y: 60 },
       group: 'nodes'
     },
     {
-      data: { id: 'FOXD1' },
-      position: { x: 589, y: 282 },
+      data: { id: 'FOXD1', group: 'tf_target', arrow: 'triangle' },
+      position: { x: 100, y: 150 },
       group: 'nodes'
     },
     {
-      data: { id: 'FOXC1' },
-      position: { x: 389, y: 182 },
+      data: { id: 'FOXC1', group: 'tf_target', arrow: 'triangle' },
+      position: { x: 280, y: 150 },
       group: 'nodes'
     },
     {
-      data: { id: 'FOXL1' },
-      position: { x: 419, y: 382 },
+      data: { id: 'FOXL1', group: 'tf_target', arrow: 'triangle' },
+      position: { x: 420, y: 150 },
       group: 'nodes',
       color: 'red'
     },
     {
-      data: { id: 'FOXI' },
-      position: { x: 449, y: 482 },
+      data: { id: 'FOXI', group: 'tf_target', arrow: 'triangle' },
+      position: { x: 580, y: 150 },
       group: 'nodes'
     },
     {
-      data: { id: 'a1', source: 'ARID1A', target: 'FOXD1' },
-      group: 'edges'
+      data: { id: 'UBIAD1', group: 'motif1', arrow: 'triangle' },
+      position: { x: 50, y: 200 },
+      group: 'nodes'
     },
     {
-      data: { id: 'a2', source: 'ARID1A', target: 'FOXC1' },
-      group: 'edges'
+      data: { id: 'DISP3', group: 'motif1', arrow: 'triangle' },
+      position: { x: 10, y: 280 },
+      group: 'nodes'
     },
     {
-      data: { id: 'a3', source: 'ARID1A', target: 'FOXL1' },
-      group: 'edges'
+      data: { id: 'RP1-69M21.2', group: 'motif1', arrow: 'triangle' },
+      position: { x: 130, y: 180 },
+      group: 'nodes'
     },
     {
-      data: { id: 'a4', source: 'ARID1A', target: 'FOXI' },
-      group: 'edges'
+      data: { id: 'NPPB', group: 'motif1', arrow: 'triangle' },
+      position: { x: 140, y: 220 },
+      group: 'nodes'
     },
     {
-      data: { id: 'a5', source: 'ARID1A', target: 'FOXD1' },
-      group: 'edges'
+      data: { id: 'AADACL4', group: 'motif1', arrow: 'triangle' },
+      position: { x: 140, y: 260 },
+      group: 'nodes'
+    },
+    {
+      data: { id: 'C1orf158', group: 'motif1', arrow: 'triangle' },
+      position: { x: 100, y: 290 },
+      group: 'nodes'
+    },
+    {
+      data: { id: 'PRAMEF20', group: 'motif1', arrow: 'triangle' },
+      position: { x: 50, y: 350 },
+      group: 'nodes'
+    },
+    {
+      data: { id: 'NPPB', group: 'motif1', arrow: 'triangle' },
+      position: { x: 140, y: 400 },
+      group: 'nodes'
+    },
+    {
+      data: { id: 'PLA2G2D', group: 'motif2', arrow: 'triangle' },
+      position: { x: 240, y: 200 },
+      group: 'nodes'
+    },
+    {
+      data: { id: 'PLA2G5', group: 'motif2', arrow: 'triangle' },
+      position: { x: 275, y: 230 },
+      group: 'nodes'
+    },
+    {
+      data: { id: 'IFFO2', group: 'motif2', arrow: 'triangle' },
+      position: { x: 210, y: 280 },
+      group: 'nodes'
+    },
+    {
+      data: { id: 'HTR6', group: 'motif2', arrow: 'triangle' },
+      position: { x: 330, y: 500 },
+      group: 'nodes'
+    },
+    {
+      data: { id: 'CAPZB', group: 'motif2', arrow: 'triangle' },
+      position: { x: 340, y: 220 },
+      group: 'nodes'
+    },
+    {
+      data: { id: 'CELA2B', group: 'motif2', arrow: 'triangle' },
+      position: { x: 340, y: 260 },
+      group: 'nodes'
+    },
+    {
+      data: { id: 'RCC2', group: 'motif2', arrow: 'triangle' },
+      position: { x: 300, y: 290 },
+      group: 'nodes'
+    },
+    {
+      data: { id: 'ACTLB', group: 'motif2', arrow: 'triangle' },
+      position: { x: 350, y: 350 },
+      group: 'nodes'
+    },
+    {
+      data: { id: 'IFFO2', group: 'motif2', arrow: 'triangle' },
+      position: { x: 540, y: 400 },
+      group: 'nodes'
+    },
+    {
+      data: { id: 'EPHAB', group: 'motif3', arrow: 'triangle' },
+      position: { x: 470, y: 350 },
+      group: 'nodes'
+    },
+    {
+      data: { id: 'C1QC', group: 'motif4', arrow: 'triangle' },
+      position: { x: 650, y: 250 },
+      group: 'nodes'
+    },
+    {
+      data: { id: 'WNT4', group: 'motif4', arrow: 'triangle' },
+      position: { x: 750, y: 250 },
+      group: 'nodes'
+    },
+    {
+      data: { id: 'a2', source: 'ARID1A', target: 'FOXC1', arrow: 'triangle' },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: { id: 'a3', source: 'ARID1A', target: 'FOXL1', arrow: 'triangle' },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: { id: 'a4', source: 'ARID1A', target: 'FOXI', arrow: 'triangle' },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: { id: 'a5', source: 'ARID1A', target: 'FOXD1', arrow: 'triangle' },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXD1',
+        target: 'UBIAD1',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXD1',
+        target: 'NPPB',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXD1',
+        target: 'AADACL4',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXD1',
+        target: 'C1orf158',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXD1',
+        target: 'DISP3',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXD1',
+        target: 'DISP3',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXD1',
+        target: 'DISP3',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXD1',
+        target: 'PRAMEF20',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXD1',
+        target: 'RP1-69M21.2',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXD1',
+        target: 'AADACL4',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXC1',
+        target: 'KAZN',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXC1',
+        target: 'KAZN',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXC1',
+        target: 'KAZN',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXC1',
+        target: 'PLA2G2D',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXC1',
+        target: 'PLA2G5',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXC1',
+        target: 'IFFO2',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXC1',
+        target: 'ACTLB',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXC1',
+        target: 'FHAD1',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXC1',
+        target: 'FHAD1',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXC1',
+        target: 'HTR6',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXC1',
+        target: 'CAPZB',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXC1',
+        target: 'RCC2',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXL1',
+        target: 'EPHAB',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXL1',
+        target: 'EPHAB',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXI',
+        target: 'EPHAB',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXC1',
+        target: 'RCC2',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXC1',
+        target: 'C1QC',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXC1',
+        target: 'C1QC',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXI',
+        target: 'C1QC',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
+    },
+    {
+      data: {
+        source: 'FOXI',
+        target: 'WNT4',
+        group: 'motif',
+        arrow: 'triangle'
+      },
+      group: 'edges',
+      arrow: 'triangle',
+      classes: 'bezier'
     }
   ],
   style: [
@@ -93,16 +528,113 @@ const config = {
       style: { 'background-color': '#666', label: 'data(id)' }
     },
     {
+      selector: 'node',
+      style: { 'background-color': '#666', label: 'data(id)' }
+    },
+    {
+      selector: 'node[type]',
+      style: {
+        label: 'data(type)'
+      }
+    },
+    {
       selector: 'edge',
       style: {
         width: 3,
-        'line-color': '#ccc',
-        'target-arrow-color': '#ccc',
-        'target-arrow-shape': 'triangle'
+        'curve-style': 'straight'
+      }
+    },
+    {
+      selector: 'edge.bezier',
+      style: {
+        'curve-style': 'bezier',
+        'control-point-step-size': 40
+      }
+    },
+    {
+      selector: 'edge[arrow]',
+      style: {
+        'target-arrow-shape': 'data(arrow)'
+      }
+    },
+    {
+      selector: 'node[group="tf_target"]',
+      style: {
+        'background-color': '#CA7853',
+        label: 'data(id)',
+        width: 32,
+        height: 32
+      }
+    },
+    {
+      selector: 'node[group="tf_source"]',
+      style: {
+        'background-color': '#1781b5',
+        label: 'data(id)',
+        width: 48,
+        height: 48,
+        'font-size': '18px'
+      }
+    },
+    {
+      selector: 'node[group="motif1"]',
+      style: {
+        'background-color': '#F17C67',
+        label: 'data(id)',
+        width: 18,
+        height: 18,
+        'font-size': '18px'
+      }
+    },
+    {
+      selector: 'node[group="motif2"]',
+      style: {
+        'background-color': '#F17C67',
+        label: 'data(id)',
+        width: 18,
+        height: 18,
+        'font-size': '18px'
+      }
+    },
+    {
+      selector: 'node[group="motif3"]',
+      style: {
+        'background-color': '#F17C67',
+        label: 'data(id)',
+        width: 18,
+        height: 18,
+        'font-size': '18px'
+      }
+    },
+    {
+      selector: 'node[group="motif4"]',
+      style: {
+        'background-color': '#F17C67',
+        label: 'data(id)',
+        width: 18,
+        height: 18,
+        'font-size': '18px'
       }
     }
   ],
-  layout: { name: 'grid', rows: 1 }
+  layout: {
+    name: 'grid',
+    idealEdgeLength: 100,
+    nodeOverlap: 20,
+    refresh: 20,
+    fit: true,
+    padding: 30,
+    randomize: false,
+    componentSpacing: 100,
+    nodeRepulsion: 400000,
+    edgeElasticity: 100,
+    nestingFactor: 5,
+    gravity: 80,
+    numIter: 1000,
+    initialTemp: 200,
+    coolingFactor: 0.95,
+    minTemp: 1.0
+  }
 }
 
 const elements = [...config.elements]
@@ -112,8 +644,15 @@ export default {
   data() {
     return {
       config,
-      elements
+      elements,
+      cyData: null
     }
+  },
+
+  mounted() {
+    axios
+      .get('https://js.cytoscape.org/demos/cose-layout/data.json')
+      .then((response) => (this.cyData = response))
   },
   methods: {
     addNode(event) {
--- plugins/highcharts.js
+++ plugins/highcharts.js
@@ -4,6 +4,12 @@ import HighchartsVue from 'highcharts-vue'
 import Sankey from 'highcharts/modules/sankey'
 import DependencyWheel from 'highcharts/modules/dependency-wheel'
 import VueCytoscape from 'vue-cytoscape'
+import cise from 'cytoscape-cise'
+import cytoscape from 'cytoscape'
+import automove from 'cytoscape-automove'
+cytoscape.use(cise)
+cytoscape.use(automove)
+
 if (typeof Highcharts === 'object') {
   Sankey(Highcharts)
   DependencyWheel(Highcharts)
@@ -11,3 +17,4 @@ if (typeof Highcharts === 'object') {
 
 Vue.use(HighchartsVue)
 Vue.use(VueCytoscape)
+Vue.use(cytoscape)
